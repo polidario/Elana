8741195cb4d61cc4e5160bbacf9d416b8819058b
bbbpppolly <71621723+bbbpppolly@users.noreply.github.com>
2021-09-26 03:12:32 +0800

    Lesson: Verifying a Webhook

--- webhook_example.php
+++ webhook_example.php
@@ -1,5 +1,27 @@
 <?php
 
-$file = fopen('store.txt', 'w');
+define('SHOPIFY_APP_SECRET', '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');
 
-fwrite($file, 'The webhook is working');
+function verify_webhook($data, $hmac_header)
+{
+  $calculated_hmac = base64_encode(hash_hmac('sha256', $data, SHOPIFY_APP_SECRET, true));
+  return hash_equals($hmac_header, $calculated_hmac);
+}
+
+
+$hmac_header = $_SERVER['HTTP_X_SHOPIFY_HMAC_SHA256'];
+$shop_domain = $_SERVER['HTTP_X_SHOPIFY_SHOP_DOMAIN'];
+
+$data = file_get_contents('php://input');
+$verified = verify_webhook($data, $hmac_header);
+
+$store_directory = 'data/stores/' . $shop_domain;
+
+if(!file_exists($store_directory)) {
+    mkdir($store_directory, 0777, TRUE);
+
+    $file = fopen($store_directory . '/store.txt', 'w');
+
+    fwrite($file, 'Is the webhook of the store verified? Status: ' . $verified);
+}
+?>
